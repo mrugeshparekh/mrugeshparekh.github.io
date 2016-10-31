690699d30b867d4608c12fa5ee905b2a2055ba3f
mrugeshparekh <mrugeshparekh@gmail.com>
2016-10-30 22:04:49 -0400

    Remove jQuery Ease-in plugin - Add scroll to function to specific div

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
     var year = new Date().getFullYear();
     $(".copyright").html(year);
+
+    //Scroll-top when click on main header and back to top button
+    $(".navbar-brand, .scroll-top > a").click(function() {
+        var topId = $(this).attr("href");
+        $("html body").animate({
+            scrollTop: 0 }
+        , 1500);    
+    });
+
+    //Scroll to specific div when click on menu link
+    $(".nav > .page-scroll > a").click(function() {
+        var href = $(this).attr("href");
+        scrollToSpecificDiv(href);
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
 });
+
+function scrollToSpecificDiv(id) {
+    $("html body").animate({
+        scrollTop: $(id).offset().top - 20 }
+        , 1500);
+}
