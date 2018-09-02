30a4d8c070e4b2b6d3474082776246455664983a
mrugeshparekh <mrugeshparekh@gmail.com>
2018-09-02 12:04:36 -0400

    Added Contact, About and, Footer components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,9 @@ import 'font-awesome/css/font-awesome.min.css';
 import Menu from './components/Menu';
 import Header from './components/Header';
 import Sections from './components/Sections';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -12,6 +15,9 @@ class App extends Component {
         <Menu />
         <Header />
         <Sections />
+        <About />
+        <Contact />
+        <Footer />
       </div>
     );
   }
