6a060ac256fdefb5e92a57b308b021e9abd3ab83
mrugeshparekh <mrugeshparekh@gmail.com>
2018-11-10 18:55:56 -0500

    Update react-scripts version 2.1.1

---
+++ precache-manifest.f7b070f99dd8c2e59c6af480f2bc5f79.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "a89afcb84e3de09af97b00ff700a914d",
+    "url": "/static/media/ENS_Logo-blue.a89afcb8.png"
+  },
+  {
+    "revision": "6636d0c38b1a13289442",
+    "url": "/static/css/main.9fbb2bb7.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c826e9232e42a2c2e20a",
+    "url": "/static/js/1.c826e923.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8fb9d35d6abfeda005681c54db5ea0dd",
+    "url": "/static/media/toycharm.8fb9d35d.png"
+  },
+  {
+    "revision": "8476ee7663c9c55681a22880a0abd8f6",
+    "url": "/static/media/spacelegend.8476ee76.png"
+  },
+  {
+    "revision": "6636d0c38b1a13289442",
+    "url": "/static/js/main.6636d0c3.chunk.js"
+  },
+  {
+    "revision": "208a4af4e2ba05c816c75e53b3141ea4",
+    "url": "/static/media/447.208a4af4.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "c826e9232e42a2c2e20a",
+    "url": "/static/css/1.1e905081.chunk.css"
+  },
+  {
+    "revision": "fc426da427debe325cc61be6954cb070",
+    "url": "/index.html"
+  }
+];
