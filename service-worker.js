1fdade2e8c10f34cee2d82fdae163d94978bb7b9
mrugeshparekh <mrugeshparekh@gmail.com>
2018-11-10 20:15:28 -0500

    Reduce 447.jpg image size without loosing quality of image

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7b070f99dd8c2e59c6af480f2bc5f79.js"
+  "/precache-manifest.988c1d510d7f5c44d3f7f98f1efb4cdc.js"
 );
 
 workbox.clientsClaim();
