25235d610426af1e7cdeb0bdae161e6219573de0
mrugeshparekh <mrugeshparekh@gmail.com>
2020-05-12 22:39:00 -0400

    Update build files

---
+++ precache-manifest.6c11cae738db366d25c2ad0892d730ff.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d0ec1bf8f93f7427e1637c87f17b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79969386d0e177abe303",
+    "url": "/static/css/2.e75b93c4.chunk.css"
+  },
+  {
+    "revision": "9b8be2a7b88c6fc71e8f",
+    "url": "/static/css/main.2045629f.chunk.css"
+  },
+  {
+    "revision": "79969386d0e177abe303",
+    "url": "/static/js/2.455e4af7.chunk.js"
+  },
+  {
+    "revision": "80097c1312432faada68f848f38927c4",
+    "url": "/static/js/2.455e4af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b8be2a7b88c6fc71e8f",
+    "url": "/static/js/main.85ad714f.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "0c4f475974fb4212ba6993f36dd81970",
+    "url": "/static/media/447.0c4f4759.jpg"
+  },
+  {
+    "revision": "a89afcb84e3de09af97b00ff700a914d",
+    "url": "/static/media/ENS_Logo-blue.a89afcb8.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8476ee7663c9c55681a22880a0abd8f6",
+    "url": "/static/media/spacelegend.8476ee76.png"
+  },
+  {
+    "revision": "8fb9d35d6abfeda005681c54db5ea0dd",
+    "url": "/static/media/toycharm.8fb9d35d.png"
+  }
+]);
